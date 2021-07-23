63220bd5556d647676d6ce2b3c88744bbf942ec7
Benyam-S <50404694+Biny-BK@users.noreply.github.com>
2021-07-23 22:33:00 +0300

    add bot url property for TelegramBotHandler struct

--- handler/handler.index.go
+++ handler/handler.index.go
@@ -8,6 +8,7 @@ import (
 type TelegramBotHandler struct {
 	BotAPIAccessPoint string
 	BotAccessToken    string
+	BotURL            string
 	ChannelName       string
 	logger            log.ILogger
 	logs              *log.LogContainer // logs can never be nil
@@ -20,10 +21,10 @@ type BotResponse struct {
 }
 
 // NewTelegramBotHandler is a function that returns a new telegram bot handler
-func NewTelegramBotHandler(botAPIAccessPoint string, botAccessToken string, telegramChannelName string,
-	botLogger log.ILogger, botLogs *log.LogContainer) *TelegramBotHandler {
+func NewTelegramBotHandler(botAPIAccessPoint string, botAccessToken string, botURL string,
+	telegramChannelName string, botLogger log.ILogger, botLogs *log.LogContainer) *TelegramBotHandler {
 	return &TelegramBotHandler{BotAPIAccessPoint: botAPIAccessPoint, BotAccessToken: botAccessToken,
-		ChannelName: telegramChannelName, logger: botLogger, logs: botLogs}
+		BotURL: botURL, ChannelName: telegramChannelName, logger: botLogger, logs: botLogs}
 }
 
 // Logging is a method that will be used internally for making logging efficient
